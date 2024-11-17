4edafb135ab0fcc0c10384a34d4a90422cfbeaaf
Anastasia <abalyeva.a@gmail.com>
2024-11-17 19:44:48 +0300

    Add files via upload

---
+++ 2 задание 7 лаба.py
@@ -0,0 +1,26 @@
+import json
+kvalf=int(input("Введите нужную вам квалификацию:"))
+find = False
+with open("dump.json", 'r', encoding='utf-8') as file: 
+    read_file = json.load(file)
+for i in read_file:
+     if i.get("model") == "data.skill":
+       if i["fields"].get("specialty") == kvalf: 
+           skill_code = i["fields"].get("code")
+           skill_title = i["fields"].get("title")
+           find=True
+
+for i in read_file:
+    if i.get("model")=="data.specialty":
+        specialty_code = i["fields"].get("code")
+        if specialty_code in skill_code:  
+             specialty_title = i["fields"].get("title")
+             specialty_educational = i["fields"].get("c_type")
+
+
+if not find:
+    print("==========Не найдено============")
+else:
+    print("===================Найдено==============")
+    print(f"{specialty_code} >> Специальность '{specialty_title}' , {specialty_educational}")
+    print(f"{skill_code} >> Квалификация '{skill_title}'")
